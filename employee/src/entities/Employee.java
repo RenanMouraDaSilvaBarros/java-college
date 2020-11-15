712409f570458fcc8e7b9f8af4112045e93fe5b6
Barrosmoura <mouramoura_rm@hotmail.com>
2020-11-15 09:57:29 -0300

    estrutra das entidades

---
+++ employee/src/entities/Employee.java
@@ -0,0 +1,32 @@
+abstract class Employee {
+
+    String name;
+    String registration;
+
+    public Employee(String name, String registration) {
+        this.name = name;
+        this.registration = registration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [name=" + name + ", registration=" + registration + "]";
+    }
+
+}

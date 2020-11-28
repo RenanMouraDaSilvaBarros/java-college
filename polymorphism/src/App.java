66a0ffa41478ce60726a1b5786b91358db4e7fca
naneraruom <naneraruom@gmail.com>
2020-11-28 16:52:54 -0300

    menu polimorfismo

--- polymorphism/src/App.java
+++ polymorphism/src/App.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 import services.ProductManagment;
 import view.Layout;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
+    public static void main(String[] args) {
+        boolean isLive = true;
+        Scanner input = new Scanner(System.in);
         ProductManagment productManagment = new ProductManagment();
 
-        productManagment.register();
-        Layout.clearScreen();
-        productManagment.register();
+        while (isLive) {
+            Layout.menu();
+            switch (input.nextInt()) {
+                case 0:
+                    isLive = false;
+                    productManagment.showAll();
+                    break;
+                case 1:
+                    productManagment.register();
+                    break;
+            }
+        }
 
     }
 
